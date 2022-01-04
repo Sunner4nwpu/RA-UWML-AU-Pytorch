7d20b2f1ffa8a00595d1e75e0d1c15518a37a920
Sunner4nwpu <sunner4nwpu@163.com>
2022-01-04 15:22:25 +0800

    RA-UWML

--- codes/scripts/run_train_disfa.sh
+++ codes/scripts/run_train_disfa.sh
@@ -1,8 +1,3 @@
 
 
-CUDA_VISIBLE_DEVICES=0 python train_disfa.py --train_num 1
-CUDA_VISIBLE_DEVICES=0 python train_disfa.py --train_num 2
-CUDA_VISIBLE_DEVICES=0 python train_disfa.py --train_num 3
-CUDA_VISIBLE_DEVICES=0 python train_disfa.py --train_num 4
-CUDA_VISIBLE_DEVICES=0 python train_disfa.py --train_num 5
-
+CUDA_VISIBLE_DEVICES=0 python train_disfa.py
